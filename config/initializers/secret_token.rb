01ec35e9bc2dd36f619c1c10c6ec128fb750ed17
Rohit Rox <link2j.roy@gmail.com>
2013-07-10 16:27:40 +0545

    rails initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudinaryBackground::Application.config.secret_token = 'b6f2c299cbb886fc617b5958696e24e04968e2b48b781a40eb91bf8272e888a962cb0ada2ea6184fedcae0a9a06e3c7efc4d93ad5a8730bda5fa441914cfc696'
